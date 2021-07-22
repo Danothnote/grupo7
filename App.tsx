772aa07ee10f4e6ce5719976f5d64eae64caa971
Daniel Salazar <52092454+Danothnote@users.noreply.github.com>
2021-07-21 19:11:22 -0500

    Splash screen, íconos y apk
    Se implementó una splash screen, se cambiaron los íconos y el nombre de la aplicación y se generó el archivo apk para probar la aplicación

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import SplashScreen from 'react-native-splash-screen';
 import LogoutNavigation from './src/navigation/logoutNavigation';
 import RoleNavigation from './src/navigation/roleNavigation';
 import { enableScreens } from 'react-native-screens';
@@ -16,6 +17,7 @@ function App() {
       setUser(userState);
       if (loading) {
         setLoading(false);
+        SplashScreen.hide();
       }
     });
   }, []);
